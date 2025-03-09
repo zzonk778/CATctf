c03d839a68d07eaec10dabca064c8182c82f4970
zzonk778 <90114659+zzonk778@users.noreply.github.com>
2025-03-09 16:07:47 +0200

    Update index.php

--- command-chall/index.php
+++ command-chall/index.php
@@ -9,7 +9,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Ping Tool</title>
+    <title>What is In your Mind</title>
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <style>
